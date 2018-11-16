b5082d08168cf0cf764199649a3bef3d5e7cccc0
Chris G <cgeorgiades27@gmail.com>
2018-11-16 10:58:38 -0500

    Updated ordering of query set to desc.

--- tracker/views.py
+++ tracker/views.py
@@ -4,7 +4,7 @@ from .models import Entry
 from django.db.models import Count, Sum, F, IntegerField
 
 def workout_log(request):
-    logs = Entry.objects.all().order_by('date_completed')
+    logs = Entry.objects.all().order_by('-date_completed')
     reps = Entry.objects.all().aggregate(total = Sum(F('reps')*F('sets'), output_field=IntegerField()))
     date = timezone.now().date()
     return render(request, 'tracker/workout_log.html', {'logs': logs, 'reps': reps, 'date': date})
