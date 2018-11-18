3185fadc8760e693cd8a0d89104f5cea638bb606
Chris G <cgeorgiades27@gmail.com>
2018-11-18 15:01:00 -0500

    added total column

--- tracker/views.py
+++ tracker/views.py
@@ -5,6 +5,6 @@ from django.db.models import Count, Sum, F, IntegerField
 
 def workout_log(request):
     logs = Entry.objects.all().order_by('-date_completed')
-    reps = Entry.objects.all().aggregate(total = Sum(F('reps')*F('sets'), output_field=IntegerField()))
+    total_reps = Entry.objects.values('user').annotate(total=Sum(F('reps')*F('sets'))).order_by('-total')
     date = timezone.now().date()
-    return render(request, 'tracker/workout_log.html', {'logs': logs, 'reps': reps, 'date': date})
+    return render(request, 'tracker/workout_log.html', {'logs': logs, 'total_reps': total_reps, 'date': date})
