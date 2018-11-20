002ea2a4fee34a5137dcad1995bb864e79ed1edb
Chris G <cgeorgiades27@gmail.com>
2018-11-20 14:36:09 -0500

    Added weekly & montly totals

--- tracker/views.py
+++ tracker/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
+from datetime import timedelta, date
 from .models import Entry
 from django.db.models import Count, Sum, F, IntegerField
 from .forms import EntryForm
@@ -16,14 +17,26 @@ def user_logs(request, user_id):
 
 def workout_log(request):
     logs = Entry.objects.all().order_by('-date_completed')
-    total_reps = Entry.objects.values('user').annotate(total=Sum(F('reps')*F('sets'))).order_by('-total')
+
+    def DateRange(n):
+        refDate = date.today() - timedelta(days = n)
+        dateSet = Entry.objects.filter(date_completed__gte=refDate)
+        return dateSet.values('user').annotate(total = Sum( F('reps') * F('sets'))).order_by('-total')
+
+    group7 = DateRange(7)
+    group30 = DateRange(30)
+    group10k = DateRange(10000)
     users = User.objects.all().order_by('id')
+
     return render(request, 'tracker/workout_log.html', 
     {
+        'group7' : group7,
+        'group30' : group30,
+        'group10k' : group10k,
         'logs': logs, 
-        'total_reps': total_reps, 
         'users': users,
     })
+        
 
 def log_new(request):
     if request.method == "POST":
