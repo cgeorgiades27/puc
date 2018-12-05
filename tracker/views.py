a64a227f35fc29cee41f55c24feed1841bcdfbc4
cgeorgiades27 <cgeorgiades27@gmail.com>
2018-12-05 08:19:14 -0500

    added per day needed

--- tracker/views.py
+++ tracker/views.py
@@ -6,14 +6,17 @@ from django.db.models import Count, Sum, F, IntegerField
 from .forms import EntryForm
 from django.contrib.auth.models import User
 
+
 def log_detail(request, pk):
     log = get_object_or_404(Entry, pk=pk)
     return render(request, 'tracker/log_detail.html', {'log': log})
+
 
 def user_logs(request, user_id):
     user_logs = Entry.objects.filter(user_id=user_id)
     user = User.objects.filter(id=user_id).values('username')
     return render(request, 'tracker/user_logs.html', {'user_logs': user_logs, 'user': user})
+
 
 def competition(request):
     users = User.objects.all().order_by('-id')
@@ -21,24 +24,28 @@ def competition(request):
     startDate = date(2018, 11, 27)
     setRange = Entry.objects.filter(date_completed__gte=startDate)
     todayRange = Entry.objects.filter(date_completed__gte=date.today())
-    todayTotal = todayRange.values('user').annotate(todayTotal = Sum( F('reps') * F('sets'))).order_by('-todayTotal')
+    todayTotal = todayRange.values('user').annotate(
+    todayTotal=Sum(F('reps') * F('sets'))).order_by('-todayTotal')
     daysRemaining = (date(2018, 12, 31) - date.today()).days
-    remainingPushUps = setRange.values('user').annotate(total = 5000 - (Sum( F('reps') * F('sets')))).order_by('total')
+    remainingPushUps = setRange.values('user').annotate(total = 5000 - (Sum(F('reps') * F('sets')))).order_by('total')
+    perDay = setRange.values('user').annotate(total = (5000 - (Sum(F('reps') * F('sets')))) / daysRemaining).order_by('total')
     return render(request, 'tracker/competition.html', {
-        'remainingPushUps' : remainingPushUps,
-        'users' : users,
-        'daysRemaining' : daysRemaining,
-        'entry' : entry,
-        'todayTotal' : todayTotal,
-        })
+        'remainingPushUps': remainingPushUps,
+        'users': users,
+        'daysRemaining': daysRemaining,
+        'entry': entry,
+        'todayTotal': todayTotal,
+        'perDay': perDay,
+    })
+
 
 def workout_log(request):
     logs = Entry.objects.all().order_by('-date_completed')
 
     def DateRange(n):
-        refDate = date.today() - timedelta(days = n)
+        refDate = date.today() - timedelta(days=n)
         dateSet = Entry.objects.filter(date_completed__gte=refDate)
-        return dateSet.values('user').annotate(total = Sum( F('reps') * F('sets'))).order_by('-total')
+        return dateSet.values('user').annotate(total=Sum(F('reps') * F('sets'))).order_by('-total')
 
     group0 = DateRange(0)
     group7 = DateRange(7)
@@ -46,16 +53,17 @@ def workout_log(request):
     group10k = DateRange(10000)
     users = User.objects.all().order_by('id')
 
-    return render(request, 'tracker/workout_log.html', 
-    {
-        'group1' : group0,
-        'group7' : group7,
-        'group30' : group30,
-        'group10k' : group10k,
-        'logs': logs, 
-        'users': users,
-    })
-        
+    return render(request, 'tracker/workout_log.html',
+                  {
+                      'group1': group0,
+                      'group7': group7,
+                      'group30': group30,
+                      'group10k': group10k,
+                      'logs': logs,
+                      'users': users,
+                  })
+
+
 def log_new(request):
     if request.method == "POST":
         form = EntryForm(request.POST)
