b133a7da70a83b1c1790f13b354a0cda6f6800c4
Chris G <cgeorgiades27@gmail.com>
2019-08-16 11:47:36 -0400

    New Bootstrap styling

--- tracker/forms.py
+++ tracker/forms.py
@@ -1,6 +1,8 @@
 from django import forms
 from .models import Entry, Workouts
 from .models import Profile
+from django.contrib.auth.forms import AuthenticationForm
+from django.utils.translation import ugettext_lazy as _
 
 class EntryForm(forms.ModelForm):
     class Meta:
@@ -23,3 +25,15 @@ class NewWorkout(forms.ModelForm):
     class Meta:
         model = Workouts
         fields = ('workout_title', 'workout_url')
+
+class BootstrapAuthenticationForm(AuthenticationForm):
+    """Authentication form which uses boostrap CSS."""
+    username = forms.CharField(max_length=254,
+        widget=forms.TextInput({
+            'class': 'form-control',
+            'placeholder': 'User name'}))
+    password = forms.CharField(label=_("Password"),
+        widget=forms.PasswordInput({
+            'class': 'form-control',
+            'placeholder':'Password'}))
+        
--- tracker/urls.py
+++ tracker/urls.py
@@ -1,23 +1,35 @@
 from django.urls import path
 from django.conf.urls import url
-from . import views
+from datetime import datetime
+from . import views, forms
+from django.contrib.auth.views import LoginView, LogoutView
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-    path('', views.workout_log, name = 'workout_log'),
-    path('log/<int:pk>', views.log_detail, name = 'log_detail'),
-    path('log/new/', views.log_new, name = 'log_new'),
-    path('user_id/<int:user_id>', views.user_logs, name = 'user_logs'),
-    path('competition', views.competition, name = 'competition'),
-    path('competition_list', views.competition_list, name = 'competition_list'),
-    path('comp_entry/<int:compName_id>', views.comp_entry, name = 'comp_entry'),
-    path('accounts/profile/', views.profile, name = 'profile'),
-    path('update_profile', views.update_profile, name = 'update_profile'),
-    url('login', auth_views.LoginView.as_view(), name='login'),
-    url('logout', auth_views.LogoutView.as_view(), name='logout'),
-    path('all_logs', views.all_logs, name = 'all_logs'),
-    path('new_workout', views.new_workout, name = 'new_workout'),
-    path('user_logs-all/<int:user_id>', views.all_user_logs, name = 'all_user_logs'),
-    path('signup', views.signup, name = 'signup'),
-    #path('url/new/', views.user_settings, name = 'user_settings'),
+    path('', views.workout_log, name='workout_log'),
+    path('log/<int:pk>', views.log_detail, name='log_detail'),
+    path('log/new/', views.log_new, name='log_new'),
+    path('user_id/<int:user_id>', views.user_logs, name='user_logs'),
+    path('competition', views.competition, name='competition'),
+    path('competition_list', views.competition_list, name='competition_list'),
+    path('comp_entry/<int:compName_id>', views.comp_entry, name='comp_entry'),
+    path('accounts/profile/', views.profile, name='profile'),
+    path('update_profile', views.update_profile, name='update_profile'),
+    path('all_logs', views.all_logs, name='all_logs'),
+    path('new_workout', views.new_workout, name='new_workout'),
+    path('user_logs-all/<int:user_id>',
+         views.all_user_logs, name='all_user_logs'),
+    path('signup', views.signup, name='signup'),
+    path('login/',
+         LoginView.as_view
+         (
+             template_name='tracker/login.html',
+             authentication_form=forms.BootstrapAuthenticationForm,
+             extra_context={
+                 'title': 'Log in',
+                 'year': datetime.now().year,
+             }
+         ),
+         name='login'),
+    path('logout/', LogoutView.as_view(next_page='/'), name='logout'),
 ]
