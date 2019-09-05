5943547103c42c889633acb766c852a29044e14a
Chris G <cgeorgiades27@gmail.com>
2019-09-05 16:39:59 -0400

    Adding new Bootstrap styling.

--- tracker/forms.py
+++ tracker/forms.py
@@ -2,6 +2,8 @@ from django import forms
 from .models import Entry, Workouts, CompMember
 from .models import Profile
 from django.contrib.admin.widgets import AdminDateWidget
+from django.contrib.auth.forms import AuthenticationForm
+from django.utils.translation import ugettext_lazy as _
 
 class EntryForm(forms.ModelForm):
     class Meta:
@@ -35,3 +37,14 @@ class JoinComp(forms.ModelForm):
     class Meta:
         model = CompMember
         fields = ('compName', 'user')
+
+class BootstrapAuthenticationForm(AuthenticationForm):
+    """Authentication form which uses boostrap CSS."""
+    username = forms.CharField(max_length=254,
+        widget=forms.TextInput({
+            'class': 'form-control',
+            'placeholder': 'User name'}))
+    password = forms.CharField(label=_("Password"),
+        widget=forms.PasswordInput({
+            'class': 'form-control',
+            'placeholder':'Password'}))
